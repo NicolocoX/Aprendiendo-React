f58456e851e59b1e4534ebdcc610a1543dba317c
NicolocoX <namsnicolas@gmail.com>
2026-07-01 21:25:09 -0400

    circulo sigue cursor

--- projects/02-cursor/src/App.jsx
+++ projects/02-cursor/src/App.jsx
@@ -1,8 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 export default function App() {
+  const [activado, setActivado] = useState(false)
+  const [posicion, setPosicion] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log("efecto")
+
+    const manejarMovimiento = (event) => {
+      const { clientX, clientY } = event
+      setPosicion({ x: clientX, y: clientY })
+    }
+
+    if (activado) {
+      window.addEventListener("pointermove", manejarMovimiento)
+    }
+    return () => {
+      window.removeEventListener("pointermove", manejarMovimiento)
+    }
+  }, [activado])
+
   return (
-    <button>Desactivar seguimiento</button>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${posicion.x}px, ${posicion.y}px)`
+      }}
+      />
+      <button onClick={() => setActivado(!activado)}>
+        {activado ? "Desactivar" : "Activar"} seguimiento
+      </button>
+    </main>
   )
 }
